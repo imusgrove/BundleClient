7d369228a2dc3e33dab95932afdb777f0e745dea
Tony <tmedinger@gmail.com>
2018-08-03 21:30:49 -0400

    deployment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,12 @@
-export const environment = {
-  production: true
-};
-
 export let APIURL = ""
-// export let APIURL = '//localhost:3000';
 
 switch (window.location.hostname) {
     // this is the deployed angular application
-    case 'tdm-bundleclient.herokuapp.com':
+    case "tdm-bundleclient.herokuapp.com":
         // this is the full url of your deployed API
-        APIURL = 'https://tdm-bundleapi.herokuapp.com/'
+        APIURL = "https://tdm-bundleapi.herokuapp.com/"
         break;
     default:
         // this is the local host name of your API
         APIURL = 'http://localhost:3000';
-}
+} 
