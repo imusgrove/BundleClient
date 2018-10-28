ea740edc834976e9376469890d760dc073633db5
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-10-27 20:30:30 -0400

    updated color scheme

--- src/app/auth/donor-signup/donor.service.ts
+++ src/app/auth/donor-signup/donor.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { HttpClient} from '@angular/common/http';
-=======
 import { HttpClient } from '@angular/common/http';
-import { APIURL } from '../../../environments/environment.prod'
->>>>>>> 61405bfd358ef7c461a45e965528f329ba3de664
-
+import { APIURL } from '../../../environments/environment.prod';
 import { Donor } from './donor';
 
 @Injectable({
@@ -19,11 +14,11 @@ export class DonorService {
 
   //nodemailer code
   addNew(usercreds) {
-    const emailid = 'email=' + usercreds.email;            
-    this.http.post('http://localhost:8888/sendemail', emailid).subscribe((data) => {
-        if (data.json().sucess) {
+    const email = {email: usercreds.email} // method 1 ----> req.body = {email: usercreds.email}
+    this.http.post('http://localhost:4200/createdonor', email).subscribe((data) => {
+        
           console.log('mail sent');
-          }
+          
      });
   }
 
@@ -36,13 +31,16 @@ export class DonorService {
     }
 
     register(donor: Donor) {
+        // original url is tdm-bundleapi
         return this.http.post(`https://tdm-bundleapi.herokuapp.com/donor/createdonor`, donor).subscribe(info => {
+            
             let data = Object.values(info);
             localStorage.setItem("token", data[2]);
             localStorage.setItem("username", data[0].donor_username);
             localStorage.setItem("id", data[0].id);
             console.log(data)
         });
+      
     }
 
     update(donor: Donor) {
